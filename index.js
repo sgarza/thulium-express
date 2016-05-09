f1dd6f2a494cba61bc85b107e231d9660a229ac8
Alvaro Cabrera <pateketrueke@gmail.com>
2016-05-09 16:15:35 -0500

    Normalize .html extension on given views; fix

--- index.js
+++ index.js
@@ -4,6 +4,14 @@ var fs = require('fs');
 
 var fileCache = {};
 
+function fixExtension(filename) {
+  if (filename.indexOf('.html') === -1) {
+    filename += '.html';
+  }
+
+  return filename;
+}
+
 module.exports = function(path, options, callback) {
   var key = path + ':thulium:string';
 
@@ -32,7 +40,7 @@ module.exports = function(path, options, callback) {
   options.partial = options.renderPartial = function renderPartial(partialPath, locals) {
     try {
 
-      var partialFile = fs.readFileSync('./' + options.settings.views + '/' + partialPath, 'utf8');
+      var partialFile = fs.readFileSync('./' + options.settings.views + '/' + fixExtension(partialPath), 'utf8');
 
       var partialTemplate = new Thulium({
         template : partialFile
@@ -54,7 +62,7 @@ module.exports = function(path, options, callback) {
   if (options.layout !== false) {
     var layoutView;
 
-    var layoutPath = './' + options.settings.views + '/layouts/' + options.layout + '.html';
+    var layoutPath = './' + options.settings.views + '/layouts/' + fixExtension(options.layout);
 
     var layoutKey = layoutPath + ':thulium:string';
 
